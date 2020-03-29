c781dced6c938bfce72d53e44fefac8bca51fa01
Homo Efficio <homo.efficio@gmail.com>
2020-03-29 11:58:19 +0900

    domain 모듈 추가

---
+++ domain/src/main/java/io/homo_efficio/ecotrip/domain/DomainApplication.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.ecotrip.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainApplication.class, args);
+    }
+
+}
---
+++ domain/src/test/java/io/homo_efficio/ecotrip/domain/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.ecotrip.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
