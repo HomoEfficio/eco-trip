d427096959fb42dc1b6b7239ba44b3c11b256026
Homo Efficio <homo.efficio@gmail.com>
2020-03-29 12:31:41 +0900

    api-admin 서브 모듈이 domain 모듈을 사용하도록 구성

--- api-admin/src/main/java/io/homo_efficio/ecotrip/api/admin/ApiAdminApplication.java
+++ api-admin/src/main/java/io/homo_efficio/ecotrip/api/admin/ApiAdminApplication.java
@@ -2,8 +2,12 @@ package io.homo_efficio.ecotrip.api.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("io.homo_efficio.ecotrip.domain")
+@EnableJpaRepositories("io.homo_efficio.ecotrip.domain")
 public class ApiAdminApplication {
 
     public static void main(String[] args) {
