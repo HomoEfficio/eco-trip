ff659dda6a8f1aef335687839bed95116f3bdb71
Homo Efficio <homo.efficio@gmail.com>
2020-03-29 12:29:18 +0900

    api-admin 서브 모듈 추가

---
+++ api-admin/src/main/java/io/homo_efficio/ecotrip/api/admin/ApiAdminApplication.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.ecotrip.api.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAdminApplication.class, args);
+    }
+
+}
---
+++ api-admin/src/test/java/io/homo_efficio/ecotrip/api/admin/ApiAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.ecotrip.api.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
