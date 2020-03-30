dd0dfa5b8ff2991f9643119a76321a379b5538e6
Homo Efficio <homo.efficio@gmail.com>
2020-03-30 23:23:51 +0900

    MessageSource, LocalValidatorFactoryBean, Message 를 빈으로 등록하는 MessageConfig 추가

---
+++ _global/src/main/java/io/homo_efficio/ecotrip/global/error/MessageConfig.java
@@ -0,0 +1,35 @@
+package io.homo_efficio.ecotrip.global.error;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2020-03-30
+ */
+@Configuration
+public class MessageConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    @Bean
+    public MessageResolver messageResolver() {
+        return new MessageResolver(messageSource());
+    }
+}
