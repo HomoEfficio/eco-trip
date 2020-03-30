adab6b0b3433e2d245e4775c3b28043a6c41a763
Homo Efficio <homo.efficio@gmail.com>
2020-03-30 22:27:13 +0900

    _global 서브 모듈 추가

---
+++ _global/src/test/java/io/homo_efficio/ecotrip/global/GlobalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.ecotrip.global;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
