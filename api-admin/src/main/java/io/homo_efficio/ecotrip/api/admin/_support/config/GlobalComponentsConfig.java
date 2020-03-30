b6d1f86b82106ea3df21f837771436e4875814d4
Homo Efficio <homo.efficio@gmail.com>
2020-03-30 23:33:51 +0900

    api-admin 모듈에서 _global 모듈의 컴포넌트를 사용할 수 있도록 @ComponentScan 추가 지역 정보 누락된 생태 여행 정보 등록 컨트롤러 테스트 통과

---
+++ api-admin/src/main/java/io/homo_efficio/ecotrip/api/admin/_support/config/GlobalComponentsConfig.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.ecotrip.api.admin._support.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2020-03-30
+ */
+@Configuration
+@ComponentScan("io.homo_efficio.ecotrip.global")
+public class GlobalComponentsConfig {
+}
