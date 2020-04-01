7910e6325b8fffb5e7ac22b91f6a3ef215669693
Homo Efficio <homo.efficio@gmail.com>
2020-04-02 04:39:35 +0900

    로그인 후 토큰 발급 확인 테스트 추가

--- api-admin/src/test/java/io/homo_efficio/ecotrip/api/admin/member/controller/MemberControllerTest.java
+++ api-admin/src/test/java/io/homo_efficio/ecotrip/api/admin/member/controller/MemberControllerTest.java
@@ -2,12 +2,18 @@ package io.homo_efficio.ecotrip.api.admin.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.homo_efficio.ecotrip.api.admin.member.param.LoginParam;
+import io.homo_efficio.ecotrip.api.admin._support.jwt.JwtAuthenticationFilter;
+import io.homo_efficio.ecotrip.api.admin._support.jwt.JwtAuthorizationFilter;
+import io.homo_efficio.ecotrip.api.admin._support.jwt.SecurityConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -32,10 +38,22 @@ class MemberControllerTest {
     @Autowired
     private WebApplicationContext ctx;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @BeforeEach
     public void beforeEach() {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, om);
+
+        jwtAuthenticationFilter.setRequiresAuthenticationRequestMatcher(
+                new AntPathRequestMatcher("/admin/members/signin", HttpMethod.POST.name())
+        );
+        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/admin/members/signin");
         mvc = MockMvcBuilders.webAppContextSetup(ctx)
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .addFilters(jwtAuthenticationFilter)
+                .addFilters(new JwtAuthorizationFilter(authenticationManager))
                 .alwaysDo(print())
                 .build();
     }
@@ -57,5 +75,32 @@ class MemberControllerTest {
 
         String token = mvcResult.getResponse().getHeader("Authorization");
         assertThat(token).isNotEmpty();
+    }
+
+    @DisplayName("로그인")
+    @Test
+    public void signin() throws Exception {
+        LoginParam member = new LoginParam("tester", "asdf!@#$");
+        String memberJson = om.writeValueAsString(member);
+
+        MvcResult mvcResult1 = mvc.perform(post("/admin/members/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(memberJson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String token1 = mvcResult1.getResponse().getHeader("Authorization");
+
+        MvcResult mvcResult2 = mvc.perform(post("/admin/members/signin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(memberJson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String token2 = mvcResult2.getResponse().getHeader("Authorization");
+        assertThat(token2).isNotEmpty();
     }
 }
