99f0840745b0f29d8bedc210ab625c5f09df4f87
Homo Efficio <homo.efficio@gmail.com>
2020-04-02 15:12:05 +0900

    토큰 리프레시

--- api-admin/src/main/java/io/homo_efficio/ecotrip/api/admin/member/controller/MemberController.java
+++ api-admin/src/main/java/io/homo_efficio/ecotrip/api/admin/member/controller/MemberController.java
@@ -1,22 +1,20 @@
 package io.homo_efficio.ecotrip.api.admin.member.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import io.homo_efficio.ecotrip.api.admin._support.jwt.JwtTokenGenerator;
 import io.homo_efficio.ecotrip.api.admin._support.jwt.SecurityConstants;
 import io.homo_efficio.ecotrip.api.admin.member.dto.LoginDto;
 import io.homo_efficio.ecotrip.api.admin.member.param.LoginParam;
 import io.homo_efficio.ecotrip.api.admin.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.List;
 
 /**
  * @author homo.efficio@gmail.com
@@ -28,6 +26,7 @@ import java.util.List;
 public class MemberController {
 
     private final MemberService memberService;
+    private final UserDetailsService userDetailsService;
 
     @PostMapping("/signup")
     public ResponseEntity<LoginDto> signup(@RequestBody @Valid LoginParam param) {
@@ -42,4 +41,15 @@ public class MemberController {
         return ResponseEntity.ok(loginDto);
     }
 
+    @GetMapping("/refresh-token")
+    public ResponseEntity<LoginDto> refreshToken(HttpServletRequest request) {
+        String oldToken = request.getHeader(SecurityConstants.AUTHORIZATION);
+        String username = JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET))
+                .build()
+                .verify(oldToken.substring(SecurityConstants.BEARER_PREFIX.length()))
+                .getSubject();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        String newToken = JwtTokenGenerator.generate(username);
+        return ResponseEntity.ok(LoginDto.of(username, null, newToken));
+    }
 }
--- api-admin/src/test/java/io/homo_efficio/ecotrip/api/admin/member/controller/MemberControllerTest.java
+++ api-admin/src/test/java/io/homo_efficio/ecotrip/api/admin/member/controller/MemberControllerTest.java
@@ -1,11 +1,13 @@
 package io.homo_efficio.ecotrip.api.admin.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.homo_efficio.ecotrip.api.admin.member.dto.LoginDto;
 import io.homo_efficio.ecotrip.api.admin.member.param.LoginParam;
 import io.homo_efficio.ecotrip.api.admin._support.jwt.JwtAuthenticationFilter;
 import io.homo_efficio.ecotrip.api.admin._support.jwt.JwtAuthorizationFilter;
 import io.homo_efficio.ecotrip.api.admin._support.jwt.SecurityConstants;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +18,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -92,7 +97,7 @@ class MemberControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
-        String token1 = mvcResult1.getResponse().getHeader("Authorization");
+        String token1 = mvcResult1.getResponse().getHeader(SecurityConstants.AUTHORIZATION);
 
         MvcResult mvcResult2 = mvc.perform(post("/admin/members/signin")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -102,7 +107,43 @@ class MemberControllerTest {
                 .andExpect(status().isOk())
                 .andReturn();
 
-        String token2 = mvcResult2.getResponse().getHeader("Authorization");
+        String token2 = mvcResult2.getResponse().getHeader(SecurityConstants.AUTHORIZATION);
         assertThat(token2).isNotEmpty();
+    }
+
+    @Disabled
+    @DisplayName("토큰 재발급")
+    @Test
+    public void refreshToken() throws Exception {
+        LoginParam member = new LoginParam("tester", "asdf!@#$");
+        String memberJson = om.writeValueAsString(member);
+
+        MvcResult mvcResult1 = mvc.perform(post("/admin/members/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(memberJson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("username").value("tester"))
+                .andExpect(jsonPath("password").isNotEmpty())
+                .andExpect(jsonPath("token").isNotEmpty())
+                .andReturn()
+        ;
+        LoginDto loginDto1 = om.readValue(mvcResult1.getResponse().getContentAsString(), LoginDto.class);
+        String token1 = loginDto1.getToken();
+
+
+        MvcResult mvcResult2 = mvc.perform(get("/admin/members/refresh-token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(SecurityConstants.AUTHORIZATION, SecurityConstants.BEARER_PREFIX + token1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("token").isNotEmpty())
+                .andReturn();
+        LoginDto loginDto2 = om.readValue(mvcResult2.getResponse().getContentAsString(), LoginDto.class);
+        String token2 = loginDto2.getToken();
+
+        assertThat(token2).isNotEqualTo(token1);
     }
 }
